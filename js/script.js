d5c37e16aea7c527e4a0f7e0ae897225887e6033
Olga Emelyanova <olga.emel@gmail.com>
2023-01-15 15:27:22 +0300

    calc added

--- js/script.js
+++ js/script.js
@@ -432,6 +432,7 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+
   // Slider
 
   let slideIndex = 1,
@@ -484,6 +485,49 @@ window.addEventListener("DOMContentLoaded", function () {
       ) {
         currentSlide(i);
       }
+    }
+  });
+
+  //calc
+
+  let persons = document.querySelectorAll(".counter-block-input")[0],
+    restDays = document.querySelectorAll(".counter-block-input")[1],
+    place = document.getElementById("select"),
+    totalValue = document.getElementById("total"),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persons.addEventListener("change", function () {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (restDays.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  restDays.addEventListener("change", function () {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  place.addEventListener("change", function () {
+    if (restDays.value == "" || persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      let a = total;
+      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
     }
   });
 });
